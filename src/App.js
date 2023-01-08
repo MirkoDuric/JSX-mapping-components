cb28d5b6bfdc5654aab8f203c94207618d494671
MirkoDuric <mirkodjuricczv@gmail.com>
2023-01-08 16:52:19 +0100

    Mapping task finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import contacts from "./contacts";
 
 function App() {
+  function createCard(contact) {
+    return (
+      <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+      />
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
     </div>
   );
 }
